93c45aa227cd684cebd27a0ca25ac0451f077ae3
srossross <srossross@gmail.com>
2013-09-11 09:38:01 -0700

    ENH: added region selector and updated for unavailable costs

--- costs.py
+++ costs.py
@@ -1,13 +1,14 @@
 
 import requests
 import json
+import sys
 
 
 
 def get_price(size):
     price = size['valueColumns'][0]['prices']['USD']
     if price == 'N/A':
-        return 0.0
+        return None
     return float(price)
 
 def mktype(ty):                           
@@ -39,13 +40,17 @@ def main():
     keyfunc = lambda key: key[1]['category']
     sgroupby = lambda iterable, keyfunc: groupby(sorted(iterable, key=keyfunc), keyfunc)
     
-    region_pricing = pricing_data['us-east']
+    region_pricing = pricing_data[sys.argv[1]]
     print 
     for cetegory, instance_types in sgroupby(instance_types.items(), keyfunc):
         print cetegory
         for instance_id, ity in instance_types:
             col1 = '%s (%s)' % (ity['name'], instance_id)
-            col2 = '$%.3f / hour' % region_pricing[ity['type']][ity['size']]
+            cost = region_pricing.get(ity['type'],{}).get(ity['size'])
+            if cost:
+                col2 = '$%.3f / hour' % region_pricing.get(ity['type'],{}).get(ity['size'])
+            else:
+                col2 = 'unavailable'
             print ' + %-35s %30s' %(col1, col2)
         print 
     
